119009d427b73f14d486c1ece7e64f7bfad1c190
markmia14 <63660531+markmia14@users.noreply.github.com>
2020-05-30 11:23:05 +0200

    first commit

---
+++ script-ma2.js
@@ -0,0 +1,79 @@
+// question 1
+
+//answer to q1
+var petArray = ["cat","cow","dog"];
+console.log(petArray);
+
+// question 2
+
+//answer to q2
+var petArray = ["cat","cow","dog"];
+console.log(petArray[1]);
+
+// question 3
+
+//answer to q3
+var petArray = ["cat","cow","dog"];
+petArray.push("sheep");
+console.log(petArray);
+
+// question 4
+
+//answer to q4
+var petArray = ["cat","cow","dog"];
+petArray.push("sheep");
+console.log(petArray.length);
+
+// question 5
+
+//answer to q5
+var catObject = {
+    name: "garfield",
+    colour: "orange",
+    age: 8
+}
+
+// question 6
+
+// answer to q6
+var catObject = {
+    name: "garfield",
+    colour: "orange",
+    age: 8
+}
+console.log(catObject.age);
+
+// question 7
+
+// answer to q7
+var catArray = ["name:garfield","colour:orange","age: 8"];
+console.log(catArray);
+
+// question 8 
+
+// answer to q8
+var catArray = [{name: "garfield"},
+                {colour: "orange"},
+                {age: 8}];
+
+for (i in catArray);{
+    console.log(catArray);
+}
+
+// question 9
+
+// answer to q9
+function logToConsole(functionOne){
+    console.log(functionOne);
+}
+
+// question 10
+
+// answer to q10
+function logToConsole(functionOne){
+    console.log(functionOne);
+}
+logToConsole("donkey");
+
+
+
